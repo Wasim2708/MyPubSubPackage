bea19847ecf1aafe7a4752ea2de24db4bf6faf79
wasim-18972 <wasim-18972@zohocorp.com>
2023-10-23 20:14:12 +0530

    first commit

--- Package.swift
+++ Package.swift
@@ -9,20 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyPubSubPackage",
-            targets: ["MyPubSubPackage"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["ZohoPubSubSDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyPubSubPackage",
-            dependencies: []),
-        .testTarget(
-            name: "MyPubSubPackageTests",
-            dependencies: ["MyPubSubPackage"]),
+        .binaryTarget(name: "ZohoPubSubSDK", url: "https://maven.zohodl.com/ZohoDesk/ZohoPubSubSDK/iOS/1.2.1/framework/ZohoPubSubSDK.zip", checksum: "ed63fbb323a8620a9a9279aad3fe7f4eb47aa917ed9e3b7341ddc0a113669824")
     ]
 )
